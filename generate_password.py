07184758243c3cf92b993c4f393f71a5e5ed80ba
Rehan Makrani <rehanmakrani727@gmail.com>
2025-03-28 11:29:46 +0530

    Added new user

--- generate_password.py
+++ generate_password.py
@@ -1,12 +1,13 @@
-import streamlit_authenticator as stauth
+import bcrypt
 
 def generate_hash(password):
-    """Generate a password hash using bcrypt"""
-    hashed_password = stauth.Hasher([password]).generate()[0]
-    return hashed_password
+    """Generate a bcrypt hash for the given password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password.decode()  # Decode to store as a string
 
 if __name__ == "__main__":
     password = input("Enter the password to hash: ")
     hashed_password = generate_hash(password)
     print(f"Hashed password: {hashed_password}")
-    print("Add this hash to your config.yaml file.") 
+    print("Add this hash to your config.yaml file.")
